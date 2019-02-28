c057c18c62707a9174f4feb7dc0ac49feff3aa08
Daniel Milner <dmilner@gmail.com>
2019-02-28 09:53:40 -0800

    Initial commit

---
+++ class-ft-textarea.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Textarea Control for the Customizer
+ *
+ * @package WordPress
+ * @subpackage Customizer
+ */
+
+if ( class_exists( 'WP_Customize_Control' ) && ! class_exists( 'FT_Textarea_Control' ) ) {
+
+	/**
+	 * FT_Textarea_Control class
+	 *
+	 * @extends WP_Customize_Control
+	 */
+	class FT_Textarea_Control extends WP_Customize_Control {
+		/**
+		 * Render the control's content
+		 */
+		public function render_content() {
+			?>
+			<label>
+				<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+				<textarea class="large-text" cols="20" rows="5" <?php $this->link(); ?>>
+					<?php echo esc_textarea( $this->value() ); ?>
+				</textarea>
+			</label>
+			<?php
+		}
+
+	}
+}
